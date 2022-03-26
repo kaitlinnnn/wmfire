7d680890c236182b7124c9ba9f6b452607a0e60b
jbcallv <jbcallv@gmail.com>
2022-03-25 22:06:52 -0400

    base project complete

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    devIndicators: {
+        buildActivity: false
+    }
 }
 
 module.exports = nextConfig
